c6234113e91ebfbecb1a1f782981ca5235448319
nettoluis <luisnetto.2013@gmail.com>
2026-01-05 11:46:40 -0300

    Quebrando uma string em bytes

---
+++ codeforces/word-capitalization.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+    "bufio"
+    "os"
+    "fmt"
+    "strings"
+    "unicode"
+    "unicode/utf8"
+)
+
+func main() {
+    reader := bufio.NewReader(os.Stdin)
+    entrada, _ := reader.ReadString('\n')
+    entrada = strings.TrimSpace(entrada)
+
+    if entrada == "" {
+        return
+    }
+
+    r, size := utf8.DecodeRuneInString(entrada)
+
+    if unicode.IsUpper(r) {
+        fmt.Println(entrada)
+        return
+    }
+
+    novaString := string(unicode.ToUpper(r)) + entrada[size:]
+
+    fmt.Println(novaString)
+}
