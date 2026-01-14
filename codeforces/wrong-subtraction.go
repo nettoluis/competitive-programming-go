fff57a06f0f0d67dabe7bb1e4dfb373dced6f072
nettoluis <luisnetto.2013@gmail.com>
2026-01-14 18:50:37 -0300

    Algoritmo simples

---
+++ codeforces/wrong-subtraction.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+    var n, k int
+
+    fmt.Scan(&n, &k)
+
+    for i := 0; i < k; i++ {
+        if n % 10 != 0 {
+            n--
+        } else {
+            n /= 10
+        }
+    }
+
+    fmt.Println(n)
+}
