be3bd2419f8ba4aa55c89032a5c5358d6b68aa5f
nettoluis <luisnetto.2013@gmail.com>
2026-01-21 08:45:30 -0300

    Manejo de strings e int para saber o menor número possível a ser formado

---
+++ codeforces/chewbacca-number.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+    "fmt"
+    "strconv"
+)
+
+func main() {
+    var numero string
+
+    fmt.Scan(&numero)
+
+    saida := ""
+    for i := 0; i < len(numero); i++ {
+        numeroInt, err := strconv.Atoi(string(numero[i]))
+        if err != nil {
+            panic(err)
+        }
+
+        if numeroInt < 5 || numeroInt == 9 {
+            saida += fmt.Sprintf("%d", numeroInt)
+        } else {
+            saida += fmt.Sprintf("%d", 9 - numeroInt)
+        }
+    }
+
+    fmt.Println(saida)
+}
