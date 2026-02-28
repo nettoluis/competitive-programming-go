1a1f337fe935c5585b52ac6d7a17ea1e5f14ba6f
nettoluis <luisnetto.2013@gmail.com>
2026-02-27 23:00:35 -0300

    add: Target Practice

--- codeforces/target-practice.go
+++ codeforces/target-practice.go
@@ -10,26 +10,26 @@ const (
 func runBorders(k int, m []string) int {
 	counter := 0
 
-	for j := k; j < len(m[k])-1-k; j++ {
+	for j := k; j < MATRIX_ORDER-1-k; j++ {
 		if string(m[k][j]) == "X" {
 			counter += k+1
 		}
 	}
 
-	for i := k; i < len(m)-1-k; i++ {
-		if string(m[i][len(m[k])-1-k]) == "X" {
+	for i := k; i < MATRIX_ORDER-1-k; i++ {
+		if string(m[i][MATRIX_ORDER-1-k]) == "X" {
 			counter += k+1
 		}
 	}
 
-	for j := len(m[k])-1-k; j > k; j-- {
-		if string(m[len(m)-1-k][j]) == "X" {
+	for j := MATRIX_ORDER-1-k; j > k; j-- {
+		if string(m[MATRIX_ORDER-1-k][j]) == "X" {
 			counter += k+1
 		}
 	}
 
 
-	for i := len(m)-1-k; i > k; i-- {
+	for i := MATRIX_ORDER-1-k; i > k; i-- {
 		if string(m[i][k]) == "X" {
 			counter += k+1
 		}
